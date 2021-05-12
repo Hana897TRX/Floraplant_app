592995222e0315e71cb10fc5187a2d1c1c21800b
CristianIBC <A01423382@itesm.mx>
2021-05-12 13:27:03 -0500

    Login + sing up added

---
+++ lib/theme.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+class CustomTheme {
+  const CustomTheme();
+
+  static const Color loginGradientStart = Color(0xFFFFFFFF);
+  static const Color loginGradientEnd = Color(0XFF1A9025);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color pink = Color(0xFFFFACED);
+
+  static const LinearGradient primaryGradient = LinearGradient(
+    colors: <Color>[loginGradientStart, loginGradientEnd],
+    stops: <double>[0.0, 1.0],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+}

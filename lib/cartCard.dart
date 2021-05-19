999f602579e2f58c57b247066eb28c5e988e1a49
Hana897TRX <nemesisdarknes897gt@gmail.com>
2021-05-19 14:55:56 -0500

    Cart v1

---
+++ lib/cartCard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CartCard extends StatelessWidget {
+  String _imgPath = "";
+  String _title = "";
+  String _price = "";
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 import 'package:floraplant_app/home_page.dart';
-=======
-import 'package:floraplant_app/pages/login_page.dart';
-import 'package:floraplant_app/pages/profile_page.dart';
-import 'package:flutter/services.dart';
->>>>>>> 05c2a087270591607dc11b20c496c1f83534930b
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
@@ -23,7 +17,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     SystemChrome.setEnabledSystemUIOverlays([]);
     return NeumorphicApp(
       debugShowCheckedModeBanner: false,
@@ -40,11 +33,6 @@ class MyApp extends StatelessWidget {
         depth: 6,
       ),
       home: MyHomePage(),
-=======
-    return MaterialApp(
-      title: 'Floramundo store',
-      home: LoginPage(),
->>>>>>> 05c2a087270591607dc11b20c496c1f83534930b
     );
   }
 }
